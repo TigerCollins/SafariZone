50f4c896aca1c02f6e312856b82354d056d53dc8
Tiger Collins <tiger.collins@outlook.com>
2020-07-28 04:04:38 +1000

    Imported auto LOD. 2nd pass of mt adorn, adorn valley and adorn river. Imported more soundtracks. Modified Desert. Imported advanced terrain.

--- Assets/Standard Assets/ObjectFader/Scripts/FadeObjectOverride.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class FadeObjectOverride : MonoBehaviour {
-    public string alphaPropertyName = "_Color";
-    public Shader fadeShader;
-}
